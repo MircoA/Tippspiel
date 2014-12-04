60250ecf95f2488e5b04d23bb4eadbc37624cc98
Mirco Altenbernd <mirco.altenbernd@mathematik.tu-dortmund.de>
2014-12-04 15:52:37 +0100

    Grafik zur Datenbakstrutur hinzugefuegt

--- refresh_matchday.php
+++ refresh_matchday.php
@@ -1,8 +1,11 @@
+<!-- Aktualisiert die Spieltagsuebersicht / Aufruf der Seite mit den entsprechenden Parametern -->
 <?php
 
+	//Liest POST-Infos aus
 	$matchday_id = $_POST['matchday_id'];
 	$season_id = $_POST['season_id'];
 
+	//Leitet entsprechend weiter
 	header("Location: matchday.php?matchday_id=$matchday_id&season_id=$season_id");
 
 ?>
--- refresh_ranking.php
+++ refresh_ranking.php
@@ -1,13 +1,17 @@
+<!-- Aktualisiert die Tipptabelle / Aufruf der Seite mit den entsprechenden Parametern -->
 <?php
 
+	//Auslesen der uebermittelten Infos
 	$start = $_POST['start'];
 	$end = $_POST['end'];
 	$season = $_POST['season'];
 
+	//Abfangen von ungueltigen Angaben (Negative Spanne)
 	if ($start > $end) {
 		die('Ung&uuml;ltige Spietagsangabe');
 	}
 
+	//Entsprechender neuer Aufruf der Seite
 	header("Location: ranking.php?season=$season&start=$start&end=$end");
 
 ?>
